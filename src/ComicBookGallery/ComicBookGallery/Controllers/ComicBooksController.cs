1611a9398a03b79d9a0c9e4ff258f30d463d17c0
srhug <stevenhughes08@gmail.com>
2018-01-30 19:37:53 -0500

    Add a view called Detail.

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,14 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-           
-                if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-                {
-                    return Redirect("/");
-                    
-                }
+            //Below lines have been commented out for the next lesson
+            // if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            //{
+            //    return Redirect("/");
 
-                return Content("Hello from the comic books controler");
+            // }
+
+            //return Content("Hello from the comic books controler");
+
+            //Returning a view. 
+
+            return View();
             
         }
     }
